da85d9f71702e43da6b9c424aa5bf48ad910cffc
Matheus Alves <99360615+MatheusAlvesSilva@users.noreply.github.com>
2023-10-29 17:42:58 +0000

    Update Main.java
    Update code Java.

--- Main.java
+++ Main.java
@@ -38,6 +38,33 @@ public class Main {
                 case 10:
                     utilidades.Exercicio_10(scan);
                     break;
+                case 11:
+                    utilidades.Exercicio_11(scan);
+                    break;
+                case 12:
+                    utilidades.Exercicio_12(scan);
+                    break;
+                case 13:
+                    utilidades.Exercicio_13(scan);
+                    break;
+                case 14:
+                    utilidades.Exercicio_14(scan);
+                    break;
+                case 15:
+                    utilidades.Exercicio_15(scan);
+                    break;
+                case 16:
+                    utilidades.Exercicio_16(scan);
+                    break;
+                case 17:
+                    utilidades.Exercicio_17(scan);
+                    break;
+                case 18:
+                    utilidades.Exercicio_18(scan);
+                    break;
+                case 19:
+                    utilidades.Exercicio_19(scan);
+                    break;
             }
         }
     }
